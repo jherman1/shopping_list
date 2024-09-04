1cf3ab866f06903a03a4997fb92b55e2fe639243
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-09-04 17:27:50 -0600

    Added a test POST method for sending data to firebase. Was successful.

--- lib/screens/new_item.dart
+++ lib/screens/new_item.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
@@ -24,14 +29,28 @@ class _NewItemScreenState extends State<NewItemScreen> {
     if (_formKey.currentState!.validate()) {
       //returns true if all validator()'s complete properly.
       _formKey.currentState!.save(); //executes all onSaved()
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
-        ),
+      final url = Uri.https(
+          'flutter-learning-c4a0f-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.name,
+        }),
       );
+      // Navigator.of(context).pop(
+      //   GroceryItem(
+      //     id: DateTime.now().toString(),
+      //     name: _enteredName,
+      //     quantity: _enteredQuantity,
+      //     category: _selectedCategory,
+      //   ),
+      // );
     }
   }
 
